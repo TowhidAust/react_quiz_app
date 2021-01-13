d9c9c95e2b043789160b55004258d4b5f5535ea8
TowhidAust <towhidaustcse33@gmail.com>
2021-01-13 16:42:09 +0600

    update on user page

--- quiz-app/src/pages/user.js
+++ quiz-app/src/pages/user.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 
 
@@ -6,35 +6,63 @@ import { useHistory } from "react-router-dom";
 export default function User() {
 
     const history = useHistory();
-    const [studentAnswer, setStudentAnswer] = useState(
+    const [questions, setQuestions] = useState(
         [
             {
                 question: 'What is the capital of Bangladesh?',
                 correctAnswer: 'Dhaka',
-                givenStudentAnswer: '',
             },
             {
                 question: 'What is 2+2?',
                 correctAnswer: '4',
-                givenStudentAnswer: '',
             },
         ]
     )
 
     const [oneSingleAnswer, setOneSingleAnswer] = useState('');
 
+
+    // Similar to componentDidMount and componentDidUpdate:
+    // useEffect(() => {
+    //     setStudentAnswer( prevState => ([...prevState, {
+    //         question: 'What is the capital of Bangladesh?',
+    //         correctAnswer: 'Dhaka',
+    //         givenStudentAnswer: oneSingleAnswer,
+    //     }]))
+    // });
+
+    useEffect(() => {
+        // setStudentAnswer([...studentAnswer, {
+        //     question: 'What is the capital of Bangladesh?',
+        //     correctAnswer: 'Dhaka',
+        //     givenStudentAnswer: oneSingleAnswer,
+        // }]);
+    }, [])
+
+   
+
     const logout = (e) => {
         history.push('/');
     }
 
      // render all questions and answer
-     const renderData = studentAnswer.map((items, index) => (
+     const renderData = questions.map((items, index) => (
         <div className="ques-and-ans-inputs" key={index}>
             <p>Q{index+1}: {items.question}</p>
-            <input type="text" placeholder="Enter your answer"/>
+            <input onChange = {(e)=>{studentAnsInputChangeHandler(e)}} type="text" placeholder="Enter your answer"/>
         </div>
     ));
 
+    const studentAnsInputChangeHandler = (e) => {
+        setOneSingleAnswer(e.target.value);
+
+        console.log(oneSingleAnswer);
+    }
+    const submitButtonClickHandler = (e) => {
+        // setOneSingleAnswer()
+        // console.log('state is', studentAnswer);
+
+    }
 
 
 
@@ -47,7 +75,7 @@ export default function User() {
                 <div className="all-ques-container">
                     <h3 className="all-ques-heading">All Questions:</h3>
                     {renderData}
-                    <button className="submit-button">Submit</button>
+                    <button onClick = {(e)=>{submitButtonClickHandler(e)}} className="submit-button">Submit</button>
                 </div>
             </div>
         </div>
