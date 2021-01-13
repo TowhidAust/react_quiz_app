3699e14626140a97fbf8bb01c6193fdf6e26e6ef
TowhidAust <towhidaustcse33@gmail.com>
2021-01-13 16:08:20 +0600

    user panel design changes

--- quiz-app/src/pages/user.js
+++ quiz-app/src/pages/user.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 
 
@@ -6,25 +6,48 @@ import { useHistory } from "react-router-dom";
 export default function User() {
 
     const history = useHistory();
+    const [studentAnswer, setStudentAnswer] = useState(
+        [
+            {
+                question: 'What is the capital of Bangladesh?',
+                correctAnswer: 'Dhaka',
+                givenStudentAnswer: '',
+            },
+            {
+                question: 'What is 2+2?',
+                correctAnswer: '4',
+                givenStudentAnswer: '',
+            },
+        ]
+    )
+
+    const [oneSingleAnswer, setOneSingleAnswer] = useState('');
 
     const logout = (e) => {
         history.push('/');
     }
 
-    return (
-        <div className="admin-container">
+     // render all questions and answer
+     const renderData = studentAnswer.map((items, index) => (
+        <div className="ques-and-ans-inputs" key={index}>
+            <p>Q{index+1}: {items.question}</p>
+            <input type="text" placeholder="Enter your answer"/>
+        </div>
+    ));
 
-            <div className="admin-inner">
+
+
+
+    return (
+        <div className="user-container">
+            <div className="user-inner">
                 <p onClick={(e)=>{logout(e)}} className="login-screen-redirect">Logout</p>
-                <h1 className="admin-heading">Admin Panel</h1>
-                <p className="sub-title">Here you can create your question and answers. First type your question and answer and click submit. You can click multiple questions.</p>
+                <h1 className="user-heading">user Panel</h1>
+                <p className="sub-title">Here you can answer the questions. If your answer is matched with the correct answer then you will get the marks otherwise the answer will be treated as wrong answer.</p>
                 <div className="all-ques-container">
                     <h3 className="all-ques-heading">All Questions:</h3>
-                    <div className="ques-and-ans">
-                        <h3>Question1: What is the capital of Bangladesh?</h3>
-                        <p>Correct Answer: Dhaka</p>
-
-                    </div>
+                    {renderData}
+                    <button className="submit-button">Submit</button>
                 </div>
             </div>
         </div>
